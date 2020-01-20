385a1453d40ab1cfc93305c4ba37134898a20b0b
Luca Herzke <l.herzke@deersoft.de>
2020-01-20 14:18:28 +0100

    First glfw test

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
 
+#include <GLFW/glfw3.h>
+#include <OpenGL/gl.h>
+
+
 int main() {
-    std::cout << "Hello World" << std::endl;
+    GLFWwindow* window;
+    if (!glfwInit()) {
+        printf("Could not init glfw!");
+    }
+
+    window = glfwCreateWindow(1270, 720, "Multi", NULL, NULL);
+
+    glfwMakeContextCurrent(window);
+
+    while (!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glfwPollEvents();
+        glfwSwapBuffers(window);
+    }
+
+    glfwDestroyWindow(window);
 
     return 0;
 }
