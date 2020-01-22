0de279f7517aeb52ed1abc3412dc9cc9159045eb
Luca Herzke <l.herzke@deersoft.de>
2020-01-22 20:13:14 +0100

    ImGui example

--- multi/imgui_helper/imgui_build_impl.cpp
+++ multi/imgui_helper/imgui_build_impl.cpp
@@ -1,2 +1,3 @@
+
 #include <examples/imgui_impl_glfw.cpp>
-#include <examples/imgui_impl_opengl2.h>
+#include <examples/imgui_impl_opengl2.cpp>
--- multi/main.cpp
+++ multi/main.cpp
@@ -8,6 +8,9 @@
 #endif
 
 #include <imgui.h>
+#include <examples/imgui_impl_glfw.h>
+#include <examples/imgui_impl_opengl2.h>
+
 
 int main() {
     GLFWwindow* window;
@@ -19,9 +22,34 @@ int main() {
 
     glfwMakeContextCurrent(window);
 
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void) io;
+
+    ImGui::StyleColorsDark();
+
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL2_Init();
+
     while (!glfwWindowShouldClose(window)) {
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         glfwPollEvents();
+
+        ImGui_ImplOpenGL2_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+        //IMGUI RENDERING
+        ///////////////////
+        ImGui::Begin("Hello ImGui");
+        ImGui::Text("It is working!");
+        ImGui::End();
+
+        ///////////////////
+        ImGui::Render();
+
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+
+        ImGui_ImplOpenGL2_RenderDrawData(ImGui::GetDrawData());
         glfwSwapBuffers(window);
     }
 
